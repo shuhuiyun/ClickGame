761075cb9b62279a1754a24cf76364f68aa31701
4419-3-6\user <wanan850405@gmail.com>
2017-12-22 22:56:04 +0800

    1.新增Enemy

---
+++ Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]//attyibute
+public class EnemyBehavior : MonoBehaviour {
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("die");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
